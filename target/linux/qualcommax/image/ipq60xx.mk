eb2bf608dbb40645fe3467678b516f944a774db9
shelden <tiancheng1101@gmail.com>
2026-08-14 14:24:17 +0800

    1
    Signed-off-by: shelden <tiancheng1101@gmail.com>

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -385,7 +385,7 @@ define Device/jdcloud_re-cs-07
 	$(call Device/EmmcImage)
 	DEVICE_VENDOR := JDCloud
 	DEVICE_MODEL := RE-CS-07
-	KERNEL_SIZE := 6144k
+	KERNEL_SIZE := 8192k
 	SOC := ipq6010
 	DEVICE_DTS_CONFIG := config@cp03-c4
 	DEVICE_PACKAGES := -ath11k-firmware-ipq6018 -ath11k-firmware-qcn9074 -kmod-ath11k -kmod-ath11k-ahb -kmod-ath11k-pci -hostapd-common -wpad-openssl
